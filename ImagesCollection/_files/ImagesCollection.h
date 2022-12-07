d9e17608c8b3c5d179b05d7fe8737391e8c9ea76
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-12-07 19:42:51 +0200

    added ImagesCollection module as a framework of workspace

---
+++ ImagesCollection/_files/ImagesCollection.h
@@ -0,0 +1,18 @@
+//
+//  ImagesCollection.h
+//  ImagesCollection
+//
+//  Created by Yurii Samoienko on 07.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImagesCollection.
+FOUNDATION_EXPORT double ImagesCollectionVersionNumber;
+
+//! Project version string for ImagesCollection.
+FOUNDATION_EXPORT const unsigned char ImagesCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImagesCollection/PublicHeader.h>
+
+
