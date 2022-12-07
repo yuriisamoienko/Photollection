5c803fd0dcc2ad1e10641188a0ee177e78d23686
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-12-07 19:36:56 +0200

    added iOS app xcode project; set the files structure

---
+++ Photollection/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photollection
+//
+//  Created by Yurii Samoienko on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
